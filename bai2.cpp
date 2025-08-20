243f0428454f74ac7cd44a238b5ece19438f9d23
luuhuy28 <luuduchuy28@gmail.com>
2025-08-20 21:22:14 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 6;
+	int b = 2;
+	int p1 = a+b;
+	int p2 = a-b;
+	int p3 = a*b;
+	int p4 = a/b;
+	cout << p1 << " " << p2 << " " << p3 << " " << p4;
+	return 0;
+}
